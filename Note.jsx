f8106d56e46053724a6a10176bd072c5fc52caf6
Shivam Pandey <85302838+shivam080802@users.noreply.github.com>
2021-06-06 19:01:51 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Note() {
+  return (
+    <div>
+      <h1>Javascript and React.js</h1>
+
+      <p> This Was an Amazing Bootcamp taken up by Shaurya Sir 
+      We have covered everything from scratch including Javascript, React.js,HTML</p>
+      </div>
+  );
+}
+export default Note;
+      
